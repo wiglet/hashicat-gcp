e6bce325ac6d6a16f6923f547507f97908cce82c
Chris <chris.griffith@db.com>
2021-11-04 13:50:37 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+module "network" {
+  source  = "app.terraform.io/Wiglet/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
